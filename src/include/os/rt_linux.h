ec62d803be695504e5a562027f47d513a0401af1
rajtan <rajeev.tandon@gmail.com>
2016-08-23 22:54:52 +0530

    Update rt_linux.h
    This script shows error in compiling 3.4.12. It seemed that the if clauses were interchanged, the definitions of kuid_t and kgid_t were introduced in later kernels and the code would not show error in higher versions. Therefore I have interchanged these and the code compiles on 3.4.12.

--- src/include/os/rt_linux.h
+++ src/include/os/rt_linux.h
@@ -280,11 +280,11 @@ typedef struct file* RTMP_OS_FD;
 typedef struct _OS_FS_INFO_
 {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3,12,0)
-	uid_t				fsuid;
-	gid_t				fsgid;
-#else
 	kuid_t				fsuid;
 	kgid_t				fsgid;
+#else
+	uid_t				fsuid;
+	gid_t				fsgid;
 #endif
 	mm_segment_t	fs;
 } OS_FS_INFO;
